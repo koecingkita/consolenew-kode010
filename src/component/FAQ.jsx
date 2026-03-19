41406b61e84e5cf65d1d3965d6a320af31a3326f
ibnudev <ibnudevQ@gmail.com>
2026-03-19 22:24:40 +0700

    refreshhh

--- src/component/FAQ.jsx
+++ src/component/FAQ.jsx
@@ -9,6 +9,7 @@ import { BiRegularFilterAlt } from "solid-icons/bi";
 import { BsSortDownAlt } from "solid-icons/bs";
 import { AiOutlineSearch } from "solid-icons/ai";
 import { FAQService } from './services/faq.service';
+import Delete from "./modals/Delete.jsx";
 
 const initialModals = { type:null, item:null, open:false}
 
@@ -141,7 +142,18 @@ return (<>
     </div>
 
     <Show when={modals().open && modals().type === 'create'}>
-    <CreateFAQ item={modals().item} onClose={closeModal} onSuccess={refetch} />
+      <CreateFAQ item={modals().item} onClose={closeModal} onSuccess={refetch} />
+    </Show>
+
+    <Show when={modals().open && modals().type === "delete"}>
+      <Delete
+        isOpen={true}
+        onClose={closeModal}
+        title="FAQ"
+        itemName={modals().item?.question}
+        onDelete={() => FAQService.delete({ layananFAQ: modals().item?.id })}
+        onSuccess={refetch}
+      />
     </Show>
   </>)
 }
